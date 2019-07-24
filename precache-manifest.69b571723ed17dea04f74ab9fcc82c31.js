202185dae4688f99585e96c663e6d91421e1b285
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-24 15:45:39 +0300

    Updates

---
+++ precache-manifest.69b571723ed17dea04f74ab9fcc82c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492a2586a7ff49f78560fe6aa4520d13",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "3b694bbaf3a76fbf3e73",
+    "url": "/goit-react-hw-02-reader/static/css/main.74bbef27.chunk.css"
+  },
+  {
+    "revision": "82069b1c7cf1167b1e30",
+    "url": "/goit-react-hw-02-reader/static/js/2.e1475378.chunk.js"
+  },
+  {
+    "revision": "3b694bbaf3a76fbf3e73",
+    "url": "/goit-react-hw-02-reader/static/js/main.bab95fa6.chunk.js"
+  },
+  {
+    "revision": "0074b703c3097bd9a1db",
+    "url": "/goit-react-hw-02-reader/static/js/runtime~main.da19d381.js"
+  }
+]);
