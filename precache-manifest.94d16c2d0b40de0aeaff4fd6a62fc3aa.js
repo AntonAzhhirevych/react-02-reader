e2d613dfb06f4c1eecade010f4c37d8f369c1ef1
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-27 18:50:22 +0300

    Updates

--- precache-manifest.94d16c2d0b40de0aeaff4fd6a62fc3aa.js
+++ precache-manifest.94d16c2d0b40de0aeaff4fd6a62fc3aa.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "492a2586a7ff49f78560fe6aa4520d13",
+    "revision": "c08131b3e64762402a35e322c480160a",
     "url": "/goit-react-hw-02-reader/index.html"
   },
   {
-    "revision": "3b694bbaf3a76fbf3e73",
-    "url": "/goit-react-hw-02-reader/static/css/main.74bbef27.chunk.css"
+    "revision": "05dc293da7eb43060768",
+    "url": "/goit-react-hw-02-reader/static/css/main.06fa4e86.chunk.css"
   },
   {
     "revision": "82069b1c7cf1167b1e30",
     "url": "/goit-react-hw-02-reader/static/js/2.e1475378.chunk.js"
   },
   {
-    "revision": "3b694bbaf3a76fbf3e73",
-    "url": "/goit-react-hw-02-reader/static/js/main.bab95fa6.chunk.js"
+    "revision": "05dc293da7eb43060768",
+    "url": "/goit-react-hw-02-reader/static/js/main.77923a7e.chunk.js"
   },
   {
     "revision": "0074b703c3097bd9a1db",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-reader/precache-manifest.69b571723ed17dea04f74ab9fcc82c31.js"
+  "/goit-react-hw-02-reader/precache-manifest.94d16c2d0b40de0aeaff4fd6a62fc3aa.js"
 );
 
 self.addEventListener('message', (event) => {
